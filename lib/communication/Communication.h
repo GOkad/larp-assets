da5d6759eebbe42b9ff69e224194bf559c892f2a
George Okadarov <george.okadarov@gmail.com>
2022-07-02 19:16:06 +0300

    Init Communication handlers

---
+++ lib/communication/Communication.h
@@ -0,0 +1,19 @@
+/**
+ * @file Communication.h
+ * @brief Main Communication Handler
+ * @version 1.0
+ * @date 2022-07-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+class Communication {
+    private:
+    public:
+        Communication();
+};
+
+Communication::Communication () {
+    
+}
