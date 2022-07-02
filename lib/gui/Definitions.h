da6f6cccfc5d7a04375241e4fb33e82ecf317122
George Okadarov <george.okadarov@gmail.com>
2022-07-02 16:07:20 +0300

    Create gui library folder

---
+++ lib/gui/Definitions.h
@@ -0,0 +1,45 @@
+/**
+ * @file Definitions.h
+ * @brief Aseet definitions and shared variables
+ * @version 1.0
+ * @date 2022-07-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+/**
+ * D1 Mini pinout
+ */
+#define GP_16    16
+#define GP_5     5
+#define GP_4     4
+#define GP_0     0
+#define GP_2     2
+#define GP_14    14
+#define GP_12    12
+#define GP_13    13
+#define GP_15    15
+
+/**
+ * ESP 12-F pinout
+ */
+#define D0  16
+#define D1  5
+#define D2  4
+#define D3  0
+#define D4  2
+#define D5  14
+#define D6  12
+#define D7  13
+#define D8  15
+
+/**
+ * ESP-NOW definitions
+ */
+#define WIFI_CHANNEL    1
