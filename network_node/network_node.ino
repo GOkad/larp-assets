eda35050e9763d452e49b60a2be0a6d085988619
George Okadarov <george.okadarov@gmail.com>
2022-07-06 16:52:17 +0300

    Fix typos and errors from merging

--- network_node/network_node.ino
+++ network_node/network_node.ino
@@ -9,18 +9,20 @@
 
 #include "Definitions.h"
 
+// Define the type of this device
 #define DEVICE_TYPE   NETWORK_NODE
 
 #include "Communication.h"
 #include "WiFiConnection.h"
 
-
+/**
+ * @brief Handle Authentication Response
+ * 
+ * @param int senderDeviceType - Type of the sender device
+ * @param String data - Data sent from device
+ */
 void handleAuthentication ( String data ) {
-    // TODO: Parse string data
-    // TODO: Handle Authentication based on Device sender type and this Device type
-    if ( DEVICE_TYPE == NETWORK_NODE  ) {
-        // TODO: Create a new Asset()
-    }
+  
 }
 
 void setup() {
